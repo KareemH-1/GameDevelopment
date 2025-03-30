6007a3053c23fcd60678702d264056968c9ad4f5
KareemHassan <kareem99710@gmail.com>
2025-03-30 17:36:23 +0200

    Add upgrade tracking and UI display for PowerUp class

--- Unity/SpaceGame/Scripts/PowerUp.cs
+++ Unity/SpaceGame/Scripts/PowerUp.cs
@@ -1,11 +1,25 @@
 //add to Power-Up prefab and make sure that it has a Collider2D (Box or Circle Collider) with "Is Trigger" enabled.
 using UnityEngine;
+using TMPro; // Import TMP
+
 
 public class PowerUp : MonoBehaviour
 {
     public float fallSpeed = 2f;
     private static int maxUpgradesPerWeapon = 2;
     private static int currentUpgrades = 0;
+    private static TextMeshProUGUI upgradeText; //Make sure to have a TMP object in the scene with the name of "Upgrade Text"
+
+    private void Start()
+    {
+
+        if (upgradeText == null)
+        {
+            upgradeText = FindFirstObjectByType<TextMeshProUGUI>(); 
+        }
+
+        UpdateUpgradeText();
+    }
 
     public static bool CanSpawnPowerUp()
     {
@@ -14,9 +28,7 @@ public class PowerUp : MonoBehaviour
 
     private void Update()
     {
-
         transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
-
 
         if (transform.position.y < -6f)
         {
@@ -26,10 +38,25 @@ public class PowerUp : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && currentUpgrades < maxUpgradesPerWeapon)
         {
-            collision.GetComponent<ShipShooting>().UpgradeWeapon(); // Upgrade weapon
+            ShipShooting shipShooting = collision.GetComponent<ShipShooting>();
+            if (shipShooting != null)
+            {
+                shipShooting.UpgradeWeapon();
+                currentUpgrades++;
+                UpdateUpgradeText();
+            }
+
             Destroy(gameObject);
+        }
+    }
+
+    private void UpdateUpgradeText()
+    {
+        if (upgradeText != null)
+        {
+            upgradeText.text = "Upgrade Level: " + currentUpgrades + " / " + maxUpgradesPerWeapon;
         }
     }
 }
